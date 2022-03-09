f37b3e2ca2eb828ae90da7407d5e368ee951d04d
CRClothier <c.r.clothier@gmail.com>
2022-03-09 02:16:14 -0500

    Update app.ja

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,7 +1,4 @@
 import '../assets/css/styles.css';
 import makeProductList from './makeProductList.js';
-import getProducts from './getProducts.js';
 
-getProducts().then(() => {
-  makeProductList();
-});
+makeProductList();
