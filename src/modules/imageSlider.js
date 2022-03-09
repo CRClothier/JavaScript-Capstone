1b31f0cfc6959a810920c03b8bdfae45bbc3dbc6
mengiefen <mengiefen@gmail.com>
2022-03-10 00:33:29 +0300

    add a function to diplay images

---
+++ src/modules/imageSlider.js
@@ -0,0 +1,29 @@
+const imageSlide = document.createElement('div');
+imageSlide.classList.add('image-slide');
+
+const getImage = async (id) => {
+  const response = await fetch(
+    `https://dummyjson.com/products/${id}`,
+  );
+  const product = await response.json();
+  return product;
+};
+
+const addSlide = (image, index) => {
+  const imageItem = document.createElement('img');
+  imageItem.classList.add(`image-${index}`);
+  imageItem.src = image;
+  imageSlide.appendChild(imageItem);
+};
+
+const constructImageSlide = (container, id) => {
+  console.log(id);
+  getImage(id).then((result) => {
+    result.images.forEach((image, index) => {
+      addSlide(image, index);
+    });
+    container.appendChild(imageSlide);
+  });
+};
+
+export default constructImageSlide;
