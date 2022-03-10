0ae4de2224738297604657e27e9057c16c25c330
mengiefen <mengiefen@gmail.com>
2022-03-11 00:36:41 +0300

    fix linter errors

--- __test__7/__mock__/fetch.js
+++ __test__7/__mock__/fetch.js
@@ -1,19 +1,17 @@
 module.exports = {
-  fetch: jest.fn(() =>
-    Promise.resolve({
-      1: [
-        {
-          username: 'User1',
-          comment: 'Sample comment1',
-          creation_date: '2022-03-10',
-        },
+  fetch: jest.fn(() => Promise.resolve({
+    1: [
+      {
+        username: 'User1',
+        comment: 'Sample comment1',
+        creation_date: '2022-03-10',
+      },
 
-        {
-          username: 'User2',
-          comment: 'Sample comment2',
-          creation_date: '2022-03-10',
-        },
-      ],
-    }),
-  ),
+      {
+        username: 'User2',
+        comment: 'Sample comment2',
+        creation_date: '2022-03-10',
+      },
+    ],
+  })),
 };
